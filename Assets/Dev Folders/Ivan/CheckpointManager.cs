b6414552915a7fa5049c73d83fdea7b6dffc05a5
Tetroit <shichinov.ivan@gmail.com>
2024-09-04 03:16:26 +0200

    melting pot v1

--- Assets/Dev Folders/Ivan/CheckpointManager.cs
+++ Assets/Dev Folders/Ivan/CheckpointManager.cs
@@ -8,35 +8,45 @@ public class CheckpointManager : MonoBehaviour
     private List<Transform> collection;
     public Transform currentCheckpoint;
     public Transform player;
-    public float reachRadius;
     public void Respawn()
     {
         //elevate above the ground slightly on respawn so u dont get stuck or smth
         player.transform.position = currentCheckpoint.position + new Vector3(0, 1, 0);
+        Health health = player.GetComponent<Health>();
+        if (health != null)
+        {
+            health.RestoreHealth();
+        }
     }
     void Start()
     {
+        Health health = player.GetComponent<Health>();
+        if (health != null )
+        {
+            player.GetComponent<Health>().OnDeath += Respawn;
+            player.GetComponent<Health>().OnCheckpointTake += CheckpointTake;
+        }
     }
 
     private void Awake()
     {
+        collection= new List<Transform>();
         foreach (Transform t in GetComponentsInChildren<Transform>())
         {
             if (t.CompareTag("Checkpoint")) collection.Add(t);
         }
-
         if (collection.Count > 0)
             currentCheckpoint = collection[0];
         
-
+        
     }
     // Update is called once per frame
     void Update()
     {
-        foreach (Transform t in collection)
-        {
-            if ((t.transform.position - player.transform.position).magnitude < reachRadius)
-                currentCheckpoint = t;
-        }
+        
+    }
+    public void CheckpointTake(Checkpoint checkpoint)
+    {
+        currentCheckpoint = checkpoint.transform;
     }
 }
